a6c85f6b1df514b0597a66d2e1dbf637b629c267
김상재 <zzaisang@lifegoeson.kr>
2020-01-30 21:40:46 +0900

    SpringSecurity.class Bean Exclude 제외.

--- src/main/java/com/yongsu/farm/FarmApplication.java
+++ src/main/java/com/yongsu/farm/FarmApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 public class FarmApplication {
 
 	public static void main(String[] args) {
