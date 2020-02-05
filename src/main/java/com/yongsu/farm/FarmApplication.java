d556345fa005d698856bb7e3393968e0d2787bcc
김상재 <zzaisang@lifegoeson.kr>
2020-02-05 19:48:25 +0900

    MainClass 빈 제외 로직 제거, API 테스트 위한 SecurityConfig 수정.

--- src/main/java/com/yongsu/farm/FarmApplication.java
+++ src/main/java/com/yongsu/farm/FarmApplication.java
@@ -2,14 +2,12 @@ package com.yongsu.farm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+@SpringBootApplication
 public class FarmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FarmApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FarmApplication.class, args);
+    }
 
 }
--- src/main/java/com/yongsu/farm/security/SecurityConfig.java
+++ src/main/java/com/yongsu/farm/security/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web
                 .ignoring()
-                .mvcMatchers("/health");
+                .mvcMatchers("/health","/v1/product/**");
     }
 
     @Override
