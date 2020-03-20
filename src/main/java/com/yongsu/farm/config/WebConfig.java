e751bec1eaaabda96fcb6f8732b6c5c0ba4002a4
이재혁 <nespot2@gmail.com>
2020-03-20 12:13:17 +0900

    add cors mapping

---
+++ src/main/java/com/yongsu/farm/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.yongsu.farm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author nespot2
+ * @version 0.0.1
+ * @since 2020/03/20
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
